20180d2c5d805d554d4a06d9e5f67d1f69990ef2
Harry Sibson <Sibson.h@etoncollege.org.uk>
2022-02-08 11:18:50 +0000

    2nd commit!!!

--- CommentCardGenerator/ContentView.swift
+++ CommentCardGenerator/ContentView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var feelingBool = false
     @State private var feelingString = "sad"
+    @State private var areaStrengthBool = false
+    @State private var areaStrengthString = "EWs"
+    @State private var areaWeaknessString = "classwork"
     var body: some View {
         Form {
             VStack {
@@ -17,7 +20,12 @@ struct ContentView: View {
                 })
             }
             VStack{
-                Text("In this subject I am feeling \(feelingString), a strong area is")
+                Button("Toggle - Area of Strength", action:{
+                    toggleStrength()
+                })
+            }
+            VStack{
+                Text("In this subject I am feeling \(feelingString); a strong area is \(areaStrengthString) and an area of weakness is \(areaWeaknessString)")
                     .padding()
             }
         }
@@ -32,6 +40,17 @@ struct ContentView: View {
             feelingString = "sad"
         }
     }
+    func toggleStrength() {
+        areaStrengthBool.toggle()
+        if areaStrengthBool == true{
+            areaStrengthString = "classwork"
+            areaWeaknessString = "EWs"
+        } else{
+            areaStrengthString = "EWs"
+            areaWeaknessString = "classwork"
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
