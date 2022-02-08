3509dbfaf60c61ad27e4c3deb07fc9d6e97c5d2b
Harry Sibson <Sibson.h@etoncollege.org.uk>
2022-02-08 10:51:46 +0000

    Initial Commit

---
+++ CommentCardGenerator/CommentCardGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardGeneratorApp.swift
+//  CommentCardGenerator
+//
+//  Created by Sibson, Harry (AGDF) on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
